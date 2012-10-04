cbd3c25ac1e325f5e241c794555d6b325c9a2095
Casey <casey@casey-pc.(none)>
2012-10-03 22:12:31 -0700

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShareSite::Application.config.secret_token = '2c1a78c0e81688d4207f1344f2e0d0f0008769fceb9fa3fe319d3472aec180c0df03052923a2c9b860cd185185d686b8676a3ed16e03284be604298a8306ad5e'
