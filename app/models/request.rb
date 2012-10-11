dbf9e47c0a5c9cc4ef91dbd78f7ad6dc067fb0bb
Casey <casey@casey-pc.(none)>
2012-10-10 21:44:50 -0700

    model relationships

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,3 +1,5 @@
 class Request < ActiveRecord::Base
   attr_accessible :date_requested, :date_wanted_back, :is_accepted, :is_lended, :item_description, :item_id, :user_id
+  belongs_to :user
+  has_one :item
 end
