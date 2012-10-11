3f71c8c22d7725902993910b6b82c24cc93220aa
Nolan Blew <nolanblew@hotmail.com>
2012-10-10 23:00:25 -0700

    Changed root in route, created action link test

--- config/routes.rb
+++ config/routes.rb
@@ -70,7 +70,7 @@ ShareSite::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+ root :to => 'users#login'
 
   # See how all your routes lay out with "rake routes"
 
