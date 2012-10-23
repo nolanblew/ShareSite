5a5eea6ea0c9c3c1c9fb24673471c4f20ce821e5
quietfanatic <lewis@wall.org>
2012-10-22 19:56:44 -0700

    Generate a mailer.

--- Gemfile
+++ Gemfile
@@ -23,6 +23,9 @@ end
 
 gem 'jquery-rails'
 
+# For email
+gem 'mail'
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 ShareSite::Application.initialize!
+
+# Set up mailer
+ActionMailer::Base.delivery_method = :file
+ActionMailer::Base.file_settings = { location: "mail_test" }
+ActionMailer::Base.raise_delivery_errors = true
