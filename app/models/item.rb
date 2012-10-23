4b3902c1cae83806299d889b68521193569acaa7
Kelly Macdonald <kmacdonald@westmont.edu>
2012-10-22 19:40:18 -0700

    item validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,10 @@ class Item < ActiveRecord::Base
   belongs_to :request
   belongs_to :user
   belongs_to :category
+  
+  validates :user_id, presence: true
+  validates :category_id, presence: true
+  validates :description, presence: true
+  validates :condition, presence: true
+  validates :name, presence: true
 end
