7be2bbd6c461081d6183e45a34c5bb6de7eab045
Casey <casey@casey-pc.(none)>
2012-10-24 16:03:08 -0700

    index page shows my items. still needs work

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,12 +15,25 @@ class ItemsController < ApplicationController
     end
   end
 
-  def delete
-    Item.find(params[:id]).destroy
-    redirect_to :action => 'list' #not sure about this line?????
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+ 
+    respond_to do |format|
+      format.html { redirect_to items_url }
+      format.json { head :no_content }
+  end
+    #Item.find(params[:id]).delete
+    #redirect_to :action => 'list' 
   end
 
-  def details
+  def show
+    @item = Item.find(params[:id])
+ 
+  respond_to do |format|
+    format.html  # show.html.erb
+    format.json  { render :json => @item }
+  end
   end
 
   #lists of all the items
@@ -33,7 +46,31 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  def index
-    @title = "Index"
+  def update
+  @item = Item.find(params[:id])
+ 
+  respond_to do |format|
+    if @item.update_attributes(params[:item])
+      format.html  { redirect_to(@item,
+                    :notice => 'Item was successfully updated.') }
+      format.json  { head :no_content }
+    else
+      format.html  { render :action => "edit" }
+      format.json  { render :json => @item.errors,
+                    :status => :unprocessable_entity }
+    end
   end
 end
+
+
+  def index
+    @title = "Index"
+    @items = Item.all
+    
+    respond_to do |format|
+      format.html
+      format.json {render :json => @items }
+    end
+  end
+end
+
