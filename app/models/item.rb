ebf8519aeff57a283e7886dec30ef6e86dc255c9
Kelly Macdonald <kmacdonald@westmont.edu>
2012-10-24 21:06:14 -0700

    category_id => category (in several files)

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,10 @@
 class Item < ActiveRecord::Base
-  attr_accessible :category_id, :condition, :description, :name, :user_id
+  attr_accessible :category, :condition, :description, :name, :user_id
   belongs_to :request
   belongs_to :user
   
   validates :user_id, presence: true
-  validates :category_id, presence: true
+  validates :category, presence: true
   validates :description, presence: true
   validates :name, presence: true
 end
