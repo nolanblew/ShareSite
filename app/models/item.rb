54249af0581b3d55b28db2a399e09480f25f95b8
Nolan Blew <nolanblew@hotmail.com>
2012-10-24 17:37:12 -0700

    Changed relational model

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ActiveRecord::Base
   attr_accessible :category_id, :condition, :description, :name, :user_id
   belongs_to :request
   belongs_to :user
-  belongs_to :category
+  has_one :category
   
   validates :user_id, presence: true
   validates :category_id, presence: true
