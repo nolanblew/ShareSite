fc03a99c3f611d0bb5d509ac6fb453ad6a1c9ca5
Kelly Macdonald <kmacdonald@westmont.edu>
2012-10-24 21:47:58 -0700

    Item validations, yay!

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,8 +3,10 @@ class Item < ActiveRecord::Base
   belongs_to :request
   belongs_to :user
   
+  before_save { |item| item.category = category.split(' ').map{|c| c.capitalize}.join(' ') }
+  
   validates :user_id, presence: true
-  validates :category, presence: true
+  validates :category, presence: true, length: {minimum: 3, maximum: 30}
   validates :description, presence: true
-  validates :name, presence: true
+  validates :name, presence: true, length: {minimum: 3, maximum: 30}
 end
