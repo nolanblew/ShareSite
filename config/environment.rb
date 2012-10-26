d8f96b26d33e362ecd805b3bbec255d06659fab3
quietfanatic <lewis@wall.org>
2012-10-25 17:10:46 -0700

    Add local configuration system (config/local.rb)

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,16 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 ShareSite::Application.initialize!
 
-# Set up mailer
+# Set up mailer - In production you should override this in config/local.rb
 ActionMailer::Base.delivery_method = :file
 ActionMailer::Base.file_settings = { location: "mail_test" }
 ActionMailer::Base.raise_delivery_errors = true
+
+
+# Require config/local.rb
+
+if FileTest.exists? "#{::Rails.root.to_s}/config/local.rb"
+  require "#{::Rails.root.to_s}/config/local.rb"
+else
+  RAILS_DEFAULT_LOGGER.warn 'Please set up config/local.rb'
+end
---
+++ config/local-sample.rb
@@ -0,0 +1,17 @@
+
+ # Copy this file to config/local.rb and tweak it.
+
+
+## Set up mail to go through SMTP (such as gmail)
+#config.action_mailer.delivery_method = :smtp
+#config.action_mailer.smtp_settings = {
+#  :address              => "smtp.gmail.com",
+#  :port                 => 587,
+#  :user_name            => '<username>',
+#  :password             => '<password>',
+#  :authentication       => 'plain',
+#  :enable_starttls_auto => true
+#}
+
+
+
